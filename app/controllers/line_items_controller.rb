90b4f9ac5898c1015ff0d46f170fe2393a1fcb1c
HienTu <hientuminh@gmail.com>
2016-07-19 11:42:07 +0700

    [Chapter 11] Highlighting Changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,7 +42,8 @@ class LineItemsController < ApplicationController
       if @line_item.save
         # format.html { redirect_to @line_item.cart,
         #   notice: 'Line item was successfully created.' }
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
