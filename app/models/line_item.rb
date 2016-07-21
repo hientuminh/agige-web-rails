db4baec1105e9280341eff9504b2e7e679e7a8d8
HienTu <hientuminh@gmail.com>
2016-07-21 15:45:24 +0700

    Creating the order Capture form

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
 	# The same with 2 model Product and Cart.
 	# Product model and Card model will describe detail.
+	belongs_to :order
   belongs_to :product
   belongs_to :cart
   def total_price
