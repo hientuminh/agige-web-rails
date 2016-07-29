ef58df7dc98e3366899f8157a7c07b87dcad4e95
HienTu <hientuminh@gmail.com>
2016-07-29 15:56:11 +0700

    Chapter 14 : Add login and add test for session

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,37 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionController::TestCase
+class SessionsControllerTest < ActionController::TestCase  
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  # test "should get create" do
+  #   get :create
+  #   assert_response :success
+  # end
+
+  # test "should get destroy" do
+  #   get :destroy
+  #   assert_response :success
+  # end
+
+  test "should login" do
+    dave = users(:one)
+    post :create, name: dave.name, password: 'secret'
+    assert_redirected_to admin_url
+    assert_equal dave.id, session[:user_id]    
   end
 
-  test "should get destroy" do
+  test "should login fail" do
+    dave = users(:one)
+    post :create, name: dave.name, password: 'wrong'
+    assert_redirected_to login_url
+  end
+
+  test "should logout" do
     get :destroy
-    assert_response :success
+    assert_redirected_to store_url
   end
 
 end
